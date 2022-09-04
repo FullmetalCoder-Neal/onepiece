54b1bcc939fbbea18b9d3272870d6e45440b97aa
Neal Zhang <neal1991@qq.com>
2022-09-04 15:46:50 +0800

    init

--- packages/d-ui-vue2/vue.config.js
+++ packages/d-ui-vue2/vue.config.js
@@ -1,4 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// vue.config.js 
+// 根据不同打包命令使用不同打包文件
+const demoBuildConfig = require('./config/config.demo')
+const npmBuildConfig = require('./config/config.npm')
+
+module.exports = process.env.ENV === 'npm' ? npmBuildConfig : demoBuildConfig
+
